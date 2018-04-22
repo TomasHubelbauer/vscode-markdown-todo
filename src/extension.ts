f6ed5a22de700bc732099b9a9a59520698517b96
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-04-22 16:51:46 +0200

    Implement a tree view with refesh, remove and toggle commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,126 @@
 'use strict';
 import MarkDownDOM, { MarkDownUnorderedListBlock } from 'markdown-dom';
-import { OutputChannel, ExtensionContext, window, workspace, Uri } from 'vscode';
+import { ExtensionContext, window, workspace, Uri, TreeDataProvider, TreeItem, Event, TextDocument, EventEmitter, TreeItemCollapsibleState, ThemeIcon, commands, Command, Range, Selection } from 'vscode';
+import * as path from 'path';
 
-let channel: OutputChannel;
-let isUpdating = false;
+type File = { type: 'file'; path: string; todos: Todo[]; };
+type Todo = { type: 'todo'; text: string; isChecked: boolean; line: number; file: File; };
+type Item = File | Todo;
 
 export async function activate(context: ExtensionContext): Promise<void> {
-    channel = window.createOutputChannel('To-Do');
-    context.subscriptions.push(channel);
+    context.subscriptions.push(commands.registerCommand('markdown-todo.openFile', (file: File) => {
+        window.showTextDocument(Uri.file(file.path), { preview: true });
+    }));
+
+    context.subscriptions.push(commands.registerCommand('markdown-todo.focusTodo', async (todo: Todo) => {
+        const textEditor = await window.showTextDocument(Uri.file(todo.file.path), { preview: true });
+        const range = textEditor.document.lineAt(todo.line).range;
+        textEditor.selection = new Selection(range.end, range.start);
+    }));
+
+    context.subscriptions.push(commands.registerCommand('markdown-todo.refreshFile', async (file: File) => {
+        const textDocument = await workspace.openTextDocument(Uri.file(file.path));
+        todoTreeDataProvider.refresh(textDocument);
+    }));
+
+    context.subscriptions.push(commands.registerCommand('markdown-todo.removeTodo', async (todo: Todo) => {
+        const textEditor = await window.showTextDocument(Uri.file(todo.file.path), { preview: true });
+        const range = textEditor.document.lineAt(todo.line).rangeIncludingLineBreak;
+        await textEditor.edit(editBuilder => {
+            editBuilder.replace(range, '');
+        });
+    }));
+
+    context.subscriptions.push(commands.registerCommand('markdown-todo.toggleTodo', async (todo: Todo) => {
+        const textEditor = await window.showTextDocument(Uri.file(todo.file.path), { preview: true });
+        const range = textEditor.document.lineAt(todo.line).range;
+        await textEditor.edit(editBuilder => {
+            editBuilder.replace(range, `- [${todo.isChecked ? ' ' : 'x'}] ${todo.text}`);
+        });
+
+        // Update view without saving the file
+        todo.isChecked = !todo.isChecked;
+        todoTreeDataProvider.refresh(textEditor.document);
+    }));
+
+    const todoTreeDataProvider = new TodoTreeDataProvider();
+    context.subscriptions.push(todoTreeDataProvider);
+    context.subscriptions.push(window.createTreeView('to-do', { treeDataProvider: todoTreeDataProvider }));
+
     workspace.onDidSaveTextDocument(async textDocument => {
         // Ignore irrelevant documents that will not affect the to-do items
         if (textDocument.uri.scheme !== 'file' || textDocument.languageId !== 'markdown') {
             return;
         }
 
-        await update();
+        todoTreeDataProvider.refresh(textDocument);
     });
-    await update();
+
+    await todoTreeDataProvider.refreshAll();
 }
 
-async function update() {
-    if (isUpdating) {
-        return;
+class TodoTreeDataProvider implements TreeDataProvider<Item> {
+    private cache: File[] = [];
+    private _onDidChangeTreeData: EventEmitter<Item | undefined> = new EventEmitter<Item | undefined>();
+    readonly onDidChangeTreeData?: Event<Item | null | undefined> | undefined = this._onDidChangeTreeData.event;
+    getTreeItem(element: Item): TreeItem | Thenable<TreeItem> {
+        switch (element.type) {
+            case 'file': {
+                const item = new TreeItem(path.parse(element.path).name, TreeItemCollapsibleState.Expanded);
+                item.command = { title: 'Open file', command: 'markdown-todo.openFile', arguments: [element] };
+                item.contextValue = 'file';
+                item.iconPath = ThemeIcon.Folder;
+                item.id = element.path;
+                item.tooltip = element.path;
+                return item;
+            }
+            case 'todo': {
+                const item = new TreeItem(element.isChecked ? 'DONE: ' + element.text : element.text);
+                item.command = { title: 'Focus todo', command: 'markdown-todo.focusTodo', arguments: [element] };
+                item.contextValue = 'todo';
+                item.iconPath = ThemeIcon.File;
+                item.id = element.file.path + ':' + element.line;
+                item.tooltip = element.text;
+                return item;
+            }
+            default: {
+                // TODO: Telemetry.
+                throw new Error(`Unexpected type ${(element as Item /* never */).type}`);
+            }
+        }
     }
 
-    isUpdating = true;
+    getChildren(element?: Item | undefined): Item[] | Thenable<Item[] | null | undefined> | null | undefined {
+        if (element === undefined) {
+            return this.cache.map(file => file);
+        }
 
-    // https://github.com/Microsoft/vscode/issues/47645
-    const files = await workspace.findFiles('**/*.md');
-    channel.clear();
-    for (const file of files) {
-        const filePath = file.fsPath.substring(workspace.getWorkspaceFolder(Uri.file(file.fsPath))!.uri.fsPath.length + 1);
-        const textDocument = await workspace.openTextDocument(file);
+        if (element.type === 'file') {
+            return element.todos;
+        }
+
+        // Todos do not have children.
+    }
+
+    async refreshAll() {
+        const files = await workspace.findFiles('**/*.md'); // https://github.com/Microsoft/vscode/issues/47645
+        for (const file of files) {
+            const textDocument = await workspace.openTextDocument(file);
+            this.refresh(textDocument);
+        }
+
+        this._onDidChangeTreeData.fire();
+    }
+
+    refresh(textDocument: TextDocument) {
+        const path = textDocument.uri.fsPath;
+        let file = this.cache.find(item => item.type === 'file' && item.path === path);
+        if (file === undefined) {
+            file = { type: 'file', path, todos: [] };
+            this.cache.push(file);
+        } else {
+            file.todos = [];
+        }
 
         for (let index = 0; index < textDocument.lineCount; index++) {
             const line = textDocument.lineAt(index);
@@ -42,15 +131,20 @@ async function update() {
                 if (block.items.length === 1) {
                     const item = block.items[0];
                     if (item.type === 'checkbox') {
-                        channel.appendLine(`${filePath}:${line.lineNumber + 1} ${item.check !== null ? 'DONE: ' : ''}${item.text.trim()}`);
+                        file.todos.push({ type: 'todo', text: item.text.trim(), isChecked: item.check !== null, file, line: line.lineNumber });
                     }
                 } else {
                     // TODO: Telemetry.
-                    console.log(`Unexpected item count ${block.items.length}.`);
+                    throw new Error(`Unexpected item count ${block.items.length}.`);
                 }
             }
         }
+
+        this._onDidChangeTreeData.fire(file);
     }
 
-    isUpdating = false;
+    dispose() {
+        delete this.cache;
+        this._onDidChangeTreeData.dispose();
+    }
 }
---
+++ todo/Figure out how to display context-menu commands as decorator icons on items.md
@@ -0,0 +1,1 @@
+# Figure out how to display context-menu commands as decorator icons on items
